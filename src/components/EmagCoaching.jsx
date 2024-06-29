8347b8d561aac2fc76bcfaa25ceaaf69d0b36b6c
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-06-30 04:38:35 +0530

    demo download testing

--- src/components/EmagCoaching.jsx
+++ src/components/EmagCoaching.jsx
@@ -2,39 +2,32 @@ import React from "react";
 
 const emagazines = [
   {
-    img: "path/to/image1.jpg",
-    name: "Book Name 1",
-    link: "http://example.com/download1",
+    name: "Tenses (English) - Success Darpan",
+    link: "https://drive.google.com/file/d/1g6KoYt9pGtCMy8JeHEBwQifV9zUw39NK/view?usp=sharing",
   },
   {
-    img: "path/to/image2.jpg",
-    name: "Book Name 2",
-    link: "http://example.com/download2",
+    name: "Spoken English (English) - Success Darpan",
+    link: "https://drive.google.com/file/d/1b8j8JAW36VupWSlIwkxUDsYGPiYnBEKW/view?usp=sharing",
   },
   {
-    img: "path/to/image3.jpg",
     name: "Book Name 3",
     link: "http://example.com/download3",
   },
   {
-    img: "path/to/image4.jpg",
     name: "Book Name 4",
     link: "http://example.com/download4",
   },
   {
-    img: "path/to/image5.jpg",
     name: "Book Name 5",
     link: "http://example.com/download5",
   },
   {
-    img: "path/to/image6.jpg",
     name: "Book Name 6",
     link: "http://example.com/download6",
   },
   {
-    img: "path/to/image6.jpg",
     name: "Book Name 7",
-    link: "http://example.com/download6",
+    link: "http://example.com/download7",
   },
 ];
 
@@ -44,17 +37,15 @@ const EmagCoaching = () => {
       <h2 className="text-2xl md:text-3xl font-bold mb-4">E-Magazines</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {emagazines.map((book, index) => (
-          <div key={index} className="bg-white rounded-lg shadow-lg p-4">
-            <img
-              src={book.img}
-              alt={book.name}
-              className="w-full h-48 object-cover mb-4 rounded-lg"
-            />
+          <div
+            key={index}
+            className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center"
+          >
             <h3 className="text-lg font-bold mb-2">{book.name}</h3>
             <a
               href={book.link}
               download
-              className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700 transition duration-300"
+              className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700 transition duration-300 mt-auto"
             >
               Download
             </a>
